e6271591960cccb7df6bfab81e9345baf88f8ad1
jojoLockLock <miffyschou@sina.com>
2017-04-27 00:57:57 +0800

    添加 添加到聊天记录 的方法

--- db/Operations.js
+++ db/Operations.js
@@ -8,7 +8,6 @@ const utils=require('../utils');
 const {isEmpty,checkArguments}=utils;
 //创建角色
 function createUser(userAccount,userPassword,userName){
-
     return new Promise((resolve,reject)=>{
         checkArguments(arguments);
 
@@ -83,7 +82,7 @@ function addToAddressList(userAccount,targetAccount) {
             //判断目标是否已经在通讯录内
             .then(result=>{
                 if(result.isExist){
-                    throw new Error()
+                    throw new Error(`targetAccount:${targetAccount} have exist in AddressList from userAccount ${userAccount}`)
                 }else{
                     return User.isExist(targetAccount);
                 }
@@ -92,6 +91,8 @@ function addToAddressList(userAccount,targetAccount) {
             .then(result=>{
                 if(result.isExist){
                     return AddressList.update({userAccount},{$push:{addressList:{targetAccount}}});
+                }else{
+                    throw new Error(`targetAccount:${targetAccount} have not exist`);
                 }
             })
             //加入成功
@@ -147,19 +148,43 @@ function createChatRecord(beforeAccount,afterAccount) {
             })
     })
 }
-
-
-
-Promise.all([createUser("1","123","one"),createUser("2","123","two")])
-    .then(()=>{
-        return addToAddressListTogether("1","2")
+//添加到聊天记录
+function addChatRecord(beforeAccount,afterAccount,senderAccount,content) {
+    return new Promise((resolve,reject)=>{
+        checkArguments(arguments);
+        ChatRecord.isExist(beforeAccount,afterAccount)
+            .then(result=>{
+                if(result.isExist){
+                    let _id=result.target._id;
+                    return ChatRecord.update({_id},{$push:{records:{senderAccount,content}}})
+                }else{
+                    throw new Error(`ChatRecord between ${beforeAccount} and ${afterAccount} have not exist`);
+                }
+            })
+            .then(()=>{
+                resolve();
+            })
+            .catch(err=>{
+                reject(err);
+        })
     })
-    .then(()=>{
-        console.info(`success`);
-    })
-    .catch(err=>{
-        console.info(err);
-    });
+}
 
+// Promise.all([createUser("1","123","one"),createUser("2","123","two")])
+//     .then(()=>{
+//         return addToAddressListTogether("1","2")
+//     })
+//     .then(()=>{
+//         console.info(`success`);
+//     })
+//     .catch(err=>{
+//         console.info(err);
+//     });
+
+// addChatRecord("1","2","1","hello").then(()=>{
+//     console.info('add success');
+// }).catch((err)=>{
+//     console.info(err);
+// });
 
 
